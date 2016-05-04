7e4a55c677fc78d4a19b3c2a118702fbfc71ec43
Mrugesh Mohapatra <me@raisedadead.com>
2016-05-05 01:15:34 +0530

    :tada: this is gonna be legendary

---
+++ cmder_mini/config/user-startup.cmd
@@ -0,0 +1,6 @@
+:: use this file to run your own startup commands 
+:: use @ in front of the command to prevent printing the command
+ 
+:: @call "C:\DevInstall\cmder_mini\vendor\git-for-windows/cmd/start-ssh-agent.cmd
+:: @set PATH=%CMDER_ROOT%\vendor\whatever;%PATH%
+ 
